ef37cdd9a6e467d5088c5a09e13633e20a4e3402
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-11-08 14:39:56 +0000

    Improve useability
    buttons enabled/disabled as appropiate, values reset on page reload, values persist when osc starts/stops and mutes/unmutes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Module for creating and maniopulating an oscillator
+// Module for creating and manipulating an oscillator
 const ctx = new AudioContext();
 const createOsc = document.getElementById('create-osc');
 const oscChannel = document.getElementById('osc-channel');
@@ -8,42 +8,108 @@ createOsc.addEventListener('click', () => {
     oscChannel.classList.remove('hidden');
     let osc;
     let gain = ctx.createGain();
+    let muted = false;
 
     const startBtn = document.getElementById('start-btn');
+
+    const stopBtn = document.getElementById('stop-btn');
+    stopBtn.setAttribute('disabled', 'disabled');
+
+    const muteBtn = document.getElementById('mute-btn');
+    muteBtn.setAttribute('disabled', 'disabled');
+
+    const unMuteBtn = document.getElementById('unmute-btn');
+
+    const gainCtrl = document.getElementById('gain-ctrl');
+    const waveTypeSelect = document.getElementById('wave-type-select');
+    const freqCtrl = document.getElementById('frequency-ctrl');
+
     startBtn.addEventListener('click', () => {
-        osc = ctx.createOscillator();
+        startBtn.setAttribute('disabled', 'disabled');
+        stopBtn.removeAttribute('disabled');
+        muteBtn.removeAttribute('disabled');
+
+        // create new oscillator every time start is clicked because an oscillator instance can only be started once
+        osc = ctx.createOscillator(); 
+
+        // set gain to current value on gain input element
+        if(!muted){
+            let gainVal = gainCtrl.value;
+            gain.gain.setValueAtTime(gainVal, ctx.currentTime);
+        }
+
+        let waveShape = waveTypeSelect.value;
+        osc.type = waveShape;
+
+        let freqVal = freqCtrl.value;
+        osc.frequency.setValueAtTime(freqVal, ctx.currentTime);
+
+        // signal chain
         osc.connect(gain);
         gain.connect(ctx.destination);
         osc.start();
     });
 
-    const stopBtn = document.getElementById('stop-btn');
-    stopBtn.addEventListener('click', () => {
+    stopBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        stopBtn.setAttribute('disabled', 'disabled');
+        startBtn.removeAttribute('disabled');
+
         osc.stop();
-        osc = null;
     });
 
-    const muteBtn = document.getElementById('mute-btn');
-    muteBtn.addEventListener('click', () => {
+    muteBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        muted = true;
+
         gain.gain.linearRampToValueAtTime(0, ctx.currentTime + 0.05);
+
+        muteBtn.classList.add('hidden');
+        unMuteBtn.classList.remove('hidden');
     });
 
-    const gainCtrl = document.getElementById('gain-ctrl');
+    unMuteBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        muted = false;
+
+        let gainVal = gainCtrl.value;
+        gain.gain.setValueAtTime(gainVal, ctx.currentTime);
+
+        muteBtn.classList.remove('hidden');
+        unMuteBtn.classList.add('hidden');
+    });
+
     gainCtrl.addEventListener('change', (e) => {
         let gainVal = e.target.value;
-        gain.gain.linearRampToValueAtTime(gainVal, ctx.currentTime + 0.05);
+        if(!muted){
+            gain.gain.linearRampToValueAtTime(gainVal, ctx.currentTime + 0.05);
+        }
+        if(gainVal == 0){
+            muteBtn.setAttribute('disabled', 'disabled')
+        } else {
+            muteBtn.removeAttribute('disabled');
+        }
     });
 
-    const waveTypeSelect = document.getElementById('wave-type-select');
     waveTypeSelect.addEventListener('change', (e) => {
         let shape = e.target.value;
         osc.type = shape;
     });
 
-    const freqCtrl = document.getElementById('frequency-ctrl');
     freqCtrl.addEventListener('change', (e) => {
         let freqVal = e.target.value;
         osc.frequency.linearRampToValueAtTime(freqVal, ctx.currentTime + 0.05);
     })
 })
 
+function getCurrentGainValue(){
+    
+}
+
+// reset all form values on page load
+window.onload = function(){
+    oscChannel.reset();
+}
