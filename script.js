7f83fe68ae4a68fe40ae24e420c1f833ac39e4c5
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-11-22 13:01:48 +0000

    Change to different gain clicking workaround

--- script.js
+++ script.js
@@ -82,9 +82,8 @@ createOsc.addEventListener('click', () => {
 
         muted = true;
 
-        gain.gain.cancelAndHoldAtTime(ctx.currentTime);
+        gain.gain.setValueAtTime(gain.gain.value, ctx.currentTime);
         gain.gain.linearRampToValueAtTime(0, ctx.currentTime + 0.05);
-        gain.gain.linearRampToValueAtTime(0, ctx.currentTime + (60 * 60 * 24 * 365 * 100));
 
         muteBtn.classList.add('hidden');
         unMuteBtn.classList.remove('hidden');
@@ -96,7 +95,8 @@ createOsc.addEventListener('click', () => {
         muted = false;
         let gainVal = gainCtrl.value;
 
-        gain.gain.linearRampToValueAtTime(gainVal, ctx.currentTime + 0.5);
+        gain.gain.setValueAtTime(gain.gain.value, ctx.currentTime);
+        gain.gain.linearRampToValueAtTime(gainVal, ctx.currentTime + 0.05);
 
         muteBtn.classList.remove('hidden');
         unMuteBtn.classList.add('hidden');
@@ -136,20 +136,42 @@ createOsc.addEventListener('click', () => {
 
  
     /*
-        GAIN CLICKING WORKAROUND
+        GAIN CLICKING WORKAROUND 1
         1. Cancel all scheduled automation at current time (scheduled automation = any previous ramp) and hold the val the automation
            was at when cancelled
         2. normal ramp from current To new value
-        3. 100 years ramp prevents gain value from reverting to a default 'previous' state because it is effectively alway mid-automation from 
-           this point
+        3. 100 years ramp - this will be a flat ramp, going from x to x, therefore holding the gain value until it is explicitly set 
+           again, in which case when go through the 3 steps again.
         ISSUE - Firefox does not support cancelAndHoldAtTime 
+        ISSUE - Still get a click the first time gain is changed
+    */
+    // gainCtrl.addEventListener('input', (e) => {
+    //     let gainVal = e.target.value;
+    //     if(!muted){
+    //         gain.gain.cancelAndHoldAtTime(ctx.currentTime);
+    //         gain.gain.linearRampToValueAtTime(gainVal, ctx.currentTime + 0.05);
+    //         gain.gain.linearRampToValueAtTime(gainVal, ctx.currentTime + (60 * 60 * 24 * 365 * 100));
+    //     }
+        
+    //     if(gainVal == 0){
+    //         muteBtn.setAttribute('disabled', 'disabled')
+    //     } else {
+    //         muteBtn.removeAttribute('disabled');
+    //     }
+    // });
+
+    /*
+        GAIN CLICKING WORKAROUND 2
+        Explicitly set value of gain to itself at current time - so linearRampToValue will treat this as the previous event
+        and start the ramp from there
+
+        ISSUE - Also doesn't work on firefox, not sure why
     */
     gainCtrl.addEventListener('input', (e) => {
         let gainVal = e.target.value;
         if(!muted){
-            gain.gain.cancelAndHoldAtTime(ctx.currentTime);
+            gain.gain.setValueAtTime(gain.gain.value, ctx.currentTime);
             gain.gain.linearRampToValueAtTime(gainVal, ctx.currentTime + 0.05);
-            gain.gain.linearRampToValueAtTime(gainVal, ctx.currentTime + (60 * 60 * 24 * 365 * 100));
         }
         
         if(gainVal == 0){
